102deaaeef1e7559aff7b3dd0b53750b7a815f04
徐秀洋 <ai52013142014@gamil.com>
2018-08-06 13:45:14 +0800

    fix

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -1,15 +1,15 @@
 # install the plugins and build the static site
 gitbook install && gitbook build
 
-# pull the latest updates
-git pull --rebase
-
 # copy the static site files into the current directory.
 cp -R _book/* .
 
 # remove 'node_modules' and '_book' directory
 git clean -fx node_modules
 git clean -fx _book
+
+# pull the latest updates
+git pull --rebase
 
 # add all files
 git add .
